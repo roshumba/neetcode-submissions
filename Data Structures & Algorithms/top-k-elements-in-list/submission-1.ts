2e520592c95e362d6beef3b2741fe1312a15be1b
Roshumba Llewellyn <roshumba.llewellyn@gmail.com>
2026-05-13 17:01:33 -0400

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        // declare object to track frequency of elements 
+        // declare result array
+        // sort elements by count using array indices
+        // from highest index, add elements to result 
+        // decrement k until it is 0
+
+        const freq: Record<string, number> = {};
+        const counts: number[][] = Array.from({length: nums.length + 1}, () => []);
+        const result: number[] = [];
+
+        nums.forEach(ele => {
+            freq[ele] = (freq[ele] || 0) + 1; 
+        })
+
+        for (const key in freq) {
+            counts[freq[key]].push(Number(key));
+        }
+
+       for (let i = counts.length - 1; i > 0 ; i-- ){
+        for (const num of counts[i]) {
+            k--;
+            result.push(num);
+            if (k === 0) return result;
+        }
+       }
+       return result;
+    }
+}
