7402e70839a59b2bec505d2f3c91cf61b398d962
Roshumba Llewellyn <roshumba.llewellyn@gmail.com>
2026-05-16 21:53:31 -0400

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        // iterate using two pointers
+        // save indices in an array and reutrn
+
+        for (let i = 0; i < numbers.length - 1; i++){
+            for (let j = i + 1; j < numbers.length; j++) {
+                if(numbers[i] + numbers[j] === target) {
+                    return [i + 1, j + 1];
+                }
+            }
+        }
+
+        return [];
+    }
+}
